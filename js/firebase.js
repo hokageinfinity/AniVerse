21ebba5049bd3e72f1351ebf0910a7602c7aa294
hokageinfinity <nateandjess1218@gmail.com>
2026-03-03 20:25:17 -0500

    Add Firebase configuration and initialization

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,12 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
 
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();
